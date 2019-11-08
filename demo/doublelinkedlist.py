a23ce21d8eab0f69f7bba85676a11d8630623d63
zaizai <zrxzaizai@163.com>
2019-11-08 11:18:00 +0800

    modify script

--- demo/doublelinkedlist.py
+++ demo/doublelinkedlist.py
@@ -8,258 +8,258 @@ import copy
 from collections import Iterable
 
 class DoubleNode:
-	def __init__(self, item, prev=None, next=None):
-		"""初始化一个双向链表节点"""
-		self.item = item
-		self.prev = prev
-		self.next = next
+    def __init__(self, item, prev=None, next=None):
+        """初始化一个双向链表节点"""
+        self.item = item
+        self.prev = prev
+        self.next = next
 
-	def __repr__(self):
-		"""格式化打印"""
-		return '{} <== {} ==> {}'.format(self.prev.item if self.prev else self.prev,
-										 self.item,
-										 self.next.item if self.next else self.next)
-	__str__ = __repr__
+    def __repr__(self):
+        """格式化打印"""
+        return '{} <== {} ==> {}'.format(self.prev.item if self.prev else self.prev,
+                                         self.item,
+                                         self.next.item if self.next else self.next)
+    __str__ = __repr__
 
 
 class DoubleLinkedList:
-	def __init__(self, iterable=None, head=None, tail=None):
-		"""初始化一个双向链表"""
-		self.head = head
-		self.tail = tail
-		self.length = 0
-		if iterable is not None:
-			if isinstance(iterable, Iterable):
-				for item in iterable:
-					self.append(item)
-			else:
-				raise TypeError('{} object is not iterable'.format(type(iterable).__name__))
+    def __init__(self, iterable=None, head=None, tail=None):
+        """初始化一个双向链表"""
+        self.head = head
+        self.tail = tail
+        self.length = 0
+        if iterable is not None:
+            if isinstance(iterable, Iterable):
+                for item in iterable:
+                    self.append(item)
+            else:
+                raise TypeError('{} object is not iterable'.format(type(iterable).__name__))
 
-	@property
-	def is_empty(self):
-		"""判断链表是否为空"""
-		return self.head is None
+    @property
+    def is_empty(self):
+        """判断链表是否为空"""
+        return self.head is None
 
-	def __len__(self):
-		"""链表长度"""
-		return self.length
+    def __len__(self):
+        """链表长度"""
+        return self.length
 
-	def __bool__(self):
-		"""空链表返回False"""
-		return False if len(self) is 0 else True
+    def __bool__(self):
+        """空链表返回False"""
+        return False if len(self) is 0 else True
 
-	def __getitem__(self, index):
-		"""获取一个值"""
-		return self.getitem(index)
+    def __getitem__(self, index):
+        """获取一个值"""
+        return self.getitem(index)
 
-	def __setitem__(self, index, value):
-		"""设置一个值"""
-		node = self.getitem(index)
-		node.item = value
+    def __setitem__(self, index, value):
+        """设置一个值"""
+        node = self.getitem(index)
+        node.item = value
 
-	def __iter__(self):
-		"""返回一个迭代器"""
-		return self.iternodes()
+    def __iter__(self):
+        """返回一个迭代器"""
+        return self.iternodes()
 
-	def __contains__(self, item):
-		"""判断元素是否在链表中"""
-		for node in self:
-			if item == node.item:
-				return True
-		else:
-			return False
+    def __contains__(self, item):
+        """判断元素是否在链表中"""
+        for node in self:
+            if item == node.item:
+                return True
+        else:
+            return False
 
-	def __add__(self, other):
-		"""链表相加"""
-		if not isinstance(other, type(self)):
-			raise TypeError('can only concatenate {0} (not "{1}") to {0}'.format(type(self).__name__, type(other).__name__))
-		newlinkedlist = copy.deepcopy(self)
-		for node in other:
-			newlinkedlist.append(node.item)
-		return newlinkedlist
+    def __add__(self, other):
+        """链表相加"""
+        if not isinstance(other, type(self)):
+            raise TypeError('can only concatenate {0} (not "{1}") to {0}'.format(type(self).__name__, type(other).__name__))
+        newlinkedlist = copy.deepcopy(self)
+        for node in other:
+            newlinkedlist.append(node.item)
+        return newlinkedlist
 
-	def __iadd__(self, iterable):
-		"""加等"""
-		if not isinstance(iterable, Iterable):
-			raise TypeError('{} object is not iterable'.format(type(iterable).__name__))
-		if iterable is self:
-			iterable = copy.deepcopy(iterable)
-		for node in iterable:
-			self.append(node.item if isinstance(iterable, type(self)) else node)
-		return self
+    def __iadd__(self, iterable):
+        """加等"""
+        if not isinstance(iterable, Iterable):
+            raise TypeError('{} object is not iterable'.format(type(iterable).__name__))
+        if iterable is self:
+            iterable = copy.deepcopy(iterable)
+        for node in iterable:
+            self.append(node.item if isinstance(iterable, type(self)) else node)
+        return self
 
-	def __mul__(self, integer):
-		"""乘法"""
-		if not isinstance(integer, int):
-			raise TypeError("can't multiply sequence by non-int of type '{}'".format(type(other).__name__))
-		if integer <= 0:
-			return type(self)()
-		newlinkedlist = type(self)()
-		for _ in range(integer):
-			for node in self:
-				newlinkedlist.append(node.item)
-		return newlinkedlist
+    def __mul__(self, integer):
+        """乘法"""
+        if not isinstance(integer, int):
+            raise TypeError("can't multiply sequence by non-int of type '{}'".format(type(other).__name__))
+        if integer <= 0:
+            return type(self)()
+        newlinkedlist = type(self)()
+        for _ in range(integer):
+            for node in self:
+                newlinkedlist.append(node.item)
+        return newlinkedlist
 
-	def __imul__(self, integer):
-		"""乘等"""
-		if not isinstance(integer, int):
-			raise TypeError("can't multiply sequence by non-int of type '{}'".format(type(other).__name__))
-		if integer <= 0:
-			self.clear()
-			return self
-		newlinkedlist = copy.deepcopy(self)
-		for _ in range(integer-1):
-			for node in newlinkedlist:
-				self.append(node.item)
-		else:
-			return self
+    def __imul__(self, integer):
+        """乘等"""
+        if not isinstance(integer, int):
+            raise TypeError("can't multiply sequence by non-int of type '{}'".format(type(other).__name__))
+        if integer <= 0:
+            self.clear()
+            return self
+        newlinkedlist = copy.deepcopy(self)
+        for _ in range(integer-1):
+            for node in newlinkedlist:
+                self.append(node.item)
+        else:
+            return self
 
-	__hash__ = None
+    __hash__ = None
 
-	def add(self, item):
-		"""链表头部增加一个节点"""
-		node = DoubleNode(item)
-		if self.is_empty:
-			self.tail = node
-		else:
-			self.head.prev = node
-			node.next = self.head
-		self.head = node
-		self.length += 1
-		
-	def append(self, item):
-		"""链表尾部追加一个节点"""
-		node = DoubleNode(item)
-		if self.is_empty:
-			self.head = node
-		else:
-			self.tail.next = node
-			node.prev = self.tail
-		self.tail = node
-		self.length += 1
+    def add(self, item):
+        """链表头部增加一个节点"""
+        node = DoubleNode(item)
+        if self.is_empty:
+            self.tail = node
+        else:
+            self.head.prev = node
+            node.next = self.head
+        self.head = node
+        self.length += 1
+        
+    def append(self, item):
+        """链表尾部追加一个节点"""
+        node = DoubleNode(item)
+        if self.is_empty:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+        self.tail = node
+        self.length += 1
 
-	def extend(self, iterable):
-		"""可迭代对象的元素来扩展列表"""
-		if not isinstance(iterable, Iterable):
-			raise TypeError('{} object is not iterable'.format(type(iterable).__name__))
-		for obj in iterable:
-			self.append(obj)
+    def extend(self, iterable):
+        """可迭代对象的元素来扩展列表"""
+        if not isinstance(iterable, Iterable):
+            raise TypeError('{} object is not iterable'.format(type(iterable).__name__))
+        for obj in iterable:
+            self.append(obj)
 
-	def reverse(self):
-		"""链表反转"""
-		if self.is_empty or len(self) is 1:
-			return
-		current = self.head
-		while current:
-			next = current.next
-			current.prev, current.next = current.next, current.prev
-			current = next
-		else:
-			self.head, self.tail = self.tail, self.head
-			
-	def iternodes(self, reverse=False):
-		"""迭代链表所有元素, 返回一个生成器对象"""
-		current = self.tail if reverse else self.head
-		while current:
-			yield current
-			current = current.prev if reverse else current.next
+    def reverse(self):
+        """链表反转"""
+        if self.is_empty or len(self) is 1:
+            return
+        current = self.head
+        while current:
+            next = current.next
+            current.prev, current.next = current.next, current.prev
+            current = next
+        else:
+            self.head, self.tail = self.tail, self.head
+            
+    def iternodes(self, reverse=False):
+        """迭代链表所有元素, 返回一个生成器对象"""
+        current = self.tail if reverse else self.head
+        while current:
+            yield current
+            current = current.prev if reverse else current.next
 
-	def getitem(self, index):
-		"""查找索引对应的值, 如果存在则返回, 否则抛出异常"""
-		if not isinstance(index, int):
-			raise TypeError('{} indices must be integers, not {}'.format(type(self).__name__, type(index).__name__))
+    def getitem(self, index):
+        """查找索引对应的值, 如果存在则返回, 否则抛出异常"""
+        if not isinstance(index, int):
+            raise TypeError('{} indices must be integers, not {}'.format(type(self).__name__, type(index).__name__))
 
-		flag = 1 if index < 0 else 0
-		for i,node in enumerate(self.iternodes(flag), flag):
-			if abs(index) == i:
-				return node
-		else:
-			raise IndexError('{} index out of range'.format(type(self).__name__))
+        flag = 1 if index < 0 else 0
+        for i,node in enumerate(self.iternodes(flag), flag):
+            if abs(index) == i:
+                return node
+        else:
+            raise IndexError('{} index out of range'.format(type(self).__name__))
 
-	def count(self, item):
-		"""查找值在链表中出现的次数"""
-		counter = 0
-		for node in self:
-			if item == node.item:
-				counter += 1
-		else:
-			return counter
+    def count(self, item):
+        """查找值在链表中出现的次数"""
+        counter = 0
+        for node in self:
+            if item == node.item:
+                counter += 1
+        else:
+            return counter
 
-	def __del_node(self, node):
-		"""删除一个节点"""
-		prev = node.prev
-		next = node.next
-		if prev is None and next is None:
-			self.head = None
-			self.tail = None
-		elif prev is None:
-			self.head = next
-			next.prev = None
-		elif next is None:
-			self.tail = prev
-			prev.next = None
-		else:
-			prev.next = next
-			next.prev = prev
+    def __del_node(self, node):
+        """删除一个节点"""
+        prev = node.prev
+        next = node.next
+        if prev is None and next is None:
+            self.head = None
+            self.tail = None
+        elif prev is None:
+            self.head = next
+            next.prev = None
+        elif next is None:
+            self.tail = prev
+            prev.next = None
+        else:
+            prev.next = next
+            next.prev = prev
 
-	def pop(self, index=None):
-		"""删除并返回索引处的节点(默认为尾部)"""
-		if self.is_empty:
-			raise IndexError('empty {}'.format(type(self).__name__))
+    def pop(self, index=None):
+        """删除并返回索引处的节点(默认为尾部)"""
+        if self.is_empty:
+            raise IndexError('empty {}'.format(type(self).__name__))
 
-		if index is None:
-			current = self.tail
-			prev = current.prev
-			if prev is None:
-				self.head = None
-				self.tail = None
-			else:
-				prev.next = None
-				self.tail = prev
-		else:
-			current = self.getitem(index)
-			self.__del_node(current)
-		self.length -= 1
-		return current.item
+        if index is None:
+            current = self.tail
+            prev = current.prev
+            if prev is None:
+                self.head = None
+                self.tail = None
+            else:
+                prev.next = None
+                self.tail = prev
+        else:
+            current = self.getitem(index)
+            self.__del_node(current)
+        self.length -= 1
+        return current.item
 
-	def insert(self, index, item):
-		"""在索引位置插入一个节点"""
-		try:
-			current = self.getitem(index)
-		except IndexError:
-			self.append(item)
-			return
+    def insert(self, index, item):
+        """在索引位置插入一个节点"""
+        try:
+            current = self.getitem(index)
+        except IndexError:
+            self.append(item)
+            return
 
-		newnode = DoubleNode(item)
-		prev = current.prev
-		if prev is None:
-			self.head = newnode
-		else:
-			prev.next = newnode
-			newnode.prev = prev
-		newnode.next = current
-		current.prev = newnode
-		self.length += 1
+        newnode = DoubleNode(item)
+        prev = current.prev
+        if prev is None:
+            self.head = newnode
+        else:
+            prev.next = newnode
+            newnode.prev = prev
+        newnode.next = current
+        current.prev = newnode
+        self.length += 1
 
-	def remove(self, item):
-		"""删除第一次出现的值"""
-		for node in self:
-			if item == node.item:
-				current = node
-				break
-		else:
-			raise ValueError('{} not in {}'.format(item, type(self).__name__))
-		self.__del_node(current)
-		self.length -= 1
+    def remove(self, item):
+        """删除第一次出现的值"""
+        for node in self:
+            if item == node.item:
+                current = node
+                break
+        else:
+            raise ValueError('{} not in {}'.format(item, type(self).__name__))
+        self.__del_node(current)
+        self.length -= 1
 
-	def clear(self):
-		"""清空链表"""
-		if not self.is_empty:
-			for node in self:
-				del node
-			self.head = None
-			self.tail = None
-			self.length = 0
+    def clear(self):
+        """清空链表"""
+        if not self.is_empty:
+            for node in self:
+                del node
+            self.head = None
+            self.tail = None
+            self.length = 0
 
 ll = DoubleLinkedList()
 ll.add('java')
--- projects/log_analysis/log_analysis.py
+++ projects/log_analysis/log_analysis.py
@@ -17,82 +17,82 @@ from functools import wraps
 import inspect
 
 def checktype(func):
-	'''函数参数类型检查'''
-	@wraps(func)
-	def _checktype(*args, **kwargs):
-		sig = inspect.signature(func)
-		params = sig.parameters
-		keys = list(params.keys())
+    '''函数参数类型检查'''
+    @wraps(func)
+    def _checktype(*args, **kwargs):
+        sig = inspect.signature(func)
+        params = sig.parameters
+        keys = list(params.keys())
 
-		def _checkparams(k,v):
-			'''检查参数类型'''
-			annotation = params[k].annotation
-			if annotation is not inspect._empty and not isinstance(v, annotation):
-				raise ValueError('{} is not {}'.format(v, annotation))	
+        def _checkparams(k,v):
+            '''检查参数类型'''
+            annotation = params[k].annotation
+            if annotation is not inspect._empty and not isinstance(v, annotation):
+                raise ValueError('{} is not {}'.format(v, annotation))    
 
-		for k,v in enumerate(args):
-			k = keys[k]
-			_checkparams(k,v)
-			
-		for k,v in kwargs.items():
-			_checkparams(k,v)
-				
-		ret = func(*args, **kwargs)
-		return ret
-	return _checktype
+        for k,v in enumerate(args):
+            k = keys[k]
+            _checkparams(k,v)
+            
+        for k,v in kwargs.items():
+            _checkparams(k,v)
+                
+        ret = func(*args, **kwargs)
+        return ret
+    return _checktype
 ##############################################################
 
 f1 = 'logfile/test.log'
 f2 = 'logfile/access.log'
 logstr = '''42.120.74.236 - - [18/Apr/2017:11:03:05 +0800] "GET /index.php?m=cron HTTP/1.1" 200 31 "http://job.magedu.com/" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36"'''
 ops = {
-	'datetime': lambda timestr: datetime.datetime.strptime(timestr, '%d/%b/%Y:%H:%M:%S %z'),
-	'status': int,
-	'body_size': int,
-	'http_user_agent': lambda useragent: parse(useragent)
+    'datetime': lambda timestr: datetime.datetime.strptime(timestr, '%d/%b/%Y:%H:%M:%S %z'),
+    'status': int,
+    'body_size': int,
+    'http_user_agent': lambda useragent: parse(useragent)
 }
 
 @checktype
 def extract(line:str) -> dict:
-	'''
-	:param line: 单行日志
-	:return: dict
-	'''
-	pattern = '(?P<remote_addr>(?:(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)) [^\s]+ [^\s]+ \[(?P<datetime>[^\[\]]+)\] "(?P<method>[^\s]+) (?P<uri>[^\s]+) (?P<protocol>[^"]+)" (?P<status>\d{3}) (?P<body_size>\d+) "(?P<referer>[^"]*)" "(?P<http_user_agent>[^"]+)"'
-	regex = re.compile(pattern)
-	matcher = regex.match(line)
-	if matcher:
-		return { k: ops.get(k, lambda x: x)(v) for k,v in matcher.groupdict().items() }
-	else:
-		raise Exception('No match', line)
+    '''
+    :param line: 单行日志
+    :return: dict
+    '''
+    pattern = '(?P<remote_addr>(?:(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)) [^\s]+ [^\s]+ \[(?P<datetime>[^\[\]]+)\] "(?P<method>[^\s]+) (?P<uri>[^\s]+) (?P<protocol>[^"]+)" (?P<status>\d{3}) (?P<body_size>\d+) "(?P<referer>[^"]*)" "(?P<http_user_agent>[^"]+)"'
+    regex = re.compile(pattern)
+    matcher = regex.match(line)
+    if matcher:
+        return { k: ops.get(k, lambda x: x)(v) for k,v in matcher.groupdict().items() }
+    else:
+        raise Exception('No match', line)
 
 @checktype
 def openfile(filename:str):
-	'''
-	打开一个文件
-	:param filename: 文件路径
-	:return: generator
-	'''
-	with open(filename) as f:
-		for line in f:
-			yield extract(line)
+    '''
+    打开一个文件
+    :param filename: 文件路径
+    :return: generator
+    '''
+    with open(filename) as f:
+        for line in f:
+            yield extract(line)
 
 @checktype
 def load(*path:str): 
-	'''
-	:param path: 文件路径
-	:return: generator
-	'''
-	for filename in path:
-		filename = Path(filename)	
-		if not filename.exists():
-			continue	
-		elif filename.is_dir():
-			for file in filename.iterdir():
-				if file.is_file():
-					yield from openfile(str(file))
-		elif filename.is_file():
-			yield from openfile(str(filename))
+    '''
+    :param path: 文件路径
+    :return: generator
+    '''
+    for filename in path:
+        filename = Path(filename)    
+        if not filename.exists():
+            continue    
+        elif filename.is_dir():
+            for file in filename.iterdir():
+                if file.is_file():
+                    yield from openfile(str(file))
+        elif filename.is_file():
+            yield from openfile(str(filename))
 
 #for i in load(f): print(i)
 
@@ -100,103 +100,103 @@ def load(*path:str):
 import random
 import time
 def source():
-	'''模拟数据源'''
-	while True:
-		yield {
-			'datetime': datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=8))), 
-			'number': random.randint(1,100)
-		}
-		time.sleep(1)
+    '''模拟数据源'''
+    while True:
+        yield {
+            'datetime': datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=8))), 
+            'number': random.randint(1,100)
+        }
+        time.sleep(1)
 
 
 ##################################################################
 @checktype
 def window(src:Queue, handler:FunctionType, width:int, interval:int, time_field:str='datetime'):
-	'''
-	:param src: 数据源
-	:param handler: 处理数据的函数
-	:param width: 查看数据的时间宽度
-	:param interval: 处理数据的时间间隔
-	:param time_field: 数据中的时间字段
-	:return: None
-	'''
-	start = datetime.datetime.strptime('1970/01/01 00:00:00 +0800', '%Y/%m/%d %H:%M:%S %z')
-	buffer = []
-	delta = datetime.timedelta(seconds=width - interval)
+    '''
+    :param src: 数据源
+    :param handler: 处理数据的函数
+    :param width: 查看数据的时间宽度
+    :param interval: 处理数据的时间间隔
+    :param time_field: 数据中的时间字段
+    :return: None
+    '''
+    start = datetime.datetime.strptime('1970/01/01 00:00:00 +0800', '%Y/%m/%d %H:%M:%S %z')
+    buffer = []
+    delta = datetime.timedelta(seconds=width - interval)
 
-	while True:
-		data = src.get()
+    while True:
+        data = src.get()
 
-		buffer.append(data)
-		current = data[time_field]
+        buffer.append(data)
+        current = data[time_field]
 
-		if (current - start).total_seconds() >= interval:
-			ret = handler(buffer)
-			print(ret)
-			start = current
+        if (current - start).total_seconds() >= interval:
+            ret = handler(buffer)
+            print(ret)
+            start = current
 
-			## buffer 处理
-			buffer = [ data for data in buffer if data[time_field] > (current - delta) ]
+            ## buffer 处理
+            buffer = [ data for data in buffer if data[time_field] > (current - delta) ]
 
 def donothing_handler(iterable):
-	print(list(map(lambda item: item['datetime'], iterable)))
+    print(list(map(lambda item: item['datetime'], iterable)))
 
 @checktype
 def status_handler(iterable:list) -> dict:
-	'''状态码分析'''
-	status = {}
-	for element in iterable:
-		key = element['status']
-		if key not in status.keys():
-			status[key] = 0
-		status[key] += 1
-	total = sum(status.values())
-	return dict(map(lambda item: (item[0], '%{:.2f}'.format(item[1] / total * 100)), status.items()))
-	#print({ k: v / total * 100 for k,v in status.items() })
-		
+    '''状态码分析'''
+    status = {}
+    for element in iterable:
+        key = element['status']
+        if key not in status.keys():
+            status[key] = 0
+        status[key] += 1
+    total = sum(status.values())
+    return dict(map(lambda item: (item[0], '%{:.2f}'.format(item[1] / total * 100)), status.items()))
+    #print({ k: v / total * 100 for k,v in status.items() })
+        
 @checktype
 def browser_handler(iterable:list) -> dict:
-	'''浏览器分析'''
-	ua_dict = defaultdict(lambda :0)
-	for item in iterable:
-		ua = item['http_user_agent']
-		key = (ua.browser.family, ua.browser.version_string)
-		ua_dict[key] += 1
+    '''浏览器分析'''
+    ua_dict = defaultdict(lambda :0)
+    for item in iterable:
+        ua = item['http_user_agent']
+        key = (ua.browser.family, ua.browser.version_string)
+        ua_dict[key] += 1
 
-	return ua_dict
-		
+    return ua_dict
+        
 
 #window(load(f), donothing_handler, 3, 2, 'datetime')
 
 ##################################################################
 @checktype
 def dispatcher(src:Iterable) -> FunctionType:
-	'''
-	数据源分发到各个消费者处理
-	:param src: 数据源(可迭代对象)
-	:return: function
-	'''
-	queues = []
-	threads = []
+    '''
+    数据源分发到各个消费者处理
+    :param src: 数据源(可迭代对象)
+    :return: function
+    '''
+    queues = []
+    threads = []
 
-	def registry(handler, width, interval):
-		'''函数注册'''
-		queue = Queue()
-		queues.append(queue)
+    def registry(handler, width, interval):
+        '''函数注册'''
+        queue = Queue()
+        queues.append(queue)
 
-		thread = threading.Thread(target=window, args=(queue, handler, width, interval))
-		threads.append(thread)
+        thread = threading.Thread(target=window, args=(queue, handler, width, interval))
+        threads.append(thread)
 
-	def run():
-		'''启动'''
-		for thread in threads:
-			thread.start()
+    def run():
+        '''启动'''
+        for thread in threads:
+            thread.start()
 
-		for data in src:
-			for queue in queues:
-				queue.put(data)	
+        for data in src:
+            for queue in queues:
+                queue.put(data)    
 
-	return registry, run
+    return registry, run
 
 registry, run = dispatcher(load(f1))
 
